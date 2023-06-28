4922557f2b4a655cb2c7eacae873fe1761fa1916
Chirag Chovatiya <chiragchovatiya92@gmail.com>
2023-06-28 17:50:36 +0530

    3 Button create and fillter star reviwes all Reviews,only star reviews,noreviews

--- JavaScript/JavaScript Ui Task 27-06-2023/sort.js
+++ JavaScript/JavaScript Ui Task 27-06-2023/sort.js
@@ -4,54 +4,83 @@ fetch("product.json")
     var result = data.result;
     var productContainer = document.getElementById("productContainer");
 
-    // result.sort((a, b) => {
-    //   var priceA = a.price[0].number; 
-    //   var priceB = b.price[0].number;
-    //   return priceA - priceB;
-    // });
-
-    result.forEach(function (product) {
-
-      // Date day month year
-      var createdAt = new Date(product.createdAt);
-      var date = createdAt.toLocaleString("default", { day: 'numeric', month: 'long', year: 'numeric' });
-
-      // Star Solid Regular
-      var card = document.createElement("div");
-      var ratingStars = "";
-      var filledStars = product.property_reviews;
-      var emptyStars = 5 - filledStars;
-
-      for (var i = 0; i < filledStars; i++) {
-        ratingStars += '<i class="icon fa-solid fa-star"></i>';
-      }
-      for (var i = 0; i < emptyStars; i++) {
-        ratingStars += '<i class="icon fa-regular fa-star"></i>';
-      }
-
-      // innerHTML 
-      var card = document.createElement("div");
-      card.innerHTML = `<div class="product-card">
-      <img class="img" src=${product.image[product.image.length - 1]} />
-      <div class="title-css">
-        <div class="product-title">${product.title}</div>
-        <div class='price'>
-          <div class="product-location">${product.location}</div>
-          <div class="product-price">${product.price.map((e) => e.number + " " + e.currency.slice(0, 1))}</div>
-        </div>
-      </div>
-      <hr />
-      <div class='rating'>
-        <div class="product-currDate">${date}</div>
-        <div class="product-rating">Rating: ${ratingStars}></div>
-      </div>
-    </div>`;
-
-      productContainer.appendChild(card);
-
+    var countDisplay = document.getElementById("All");
+    var totalCount = result.length;
+    countDisplay.innerHTML = `Product List ${totalCount}`;
+    countDisplay.addEventListener("click", function () {
+      renderProductCards(result);
     });
 
-  })
-  .catch((error) => console.log(error));
+    var clickReviews = document.getElementById("reviews");
+    var emptyReviews = result.filter(function (product) {
+      return product.property_reviews > 0;
+    });
+    clickReviews.innerHTML = `Reviews: ${emptyReviews.length}`;
+    clickReviews.addEventListener("click", function () {
+      renderProductCards(emptyReviews);
+    });
 
+    var clickShowBadReview = document.getElementById("showBadReviews");
+        var badReviews = result.filter(function (product) {
+          return product.property_reviews < 5;
+        });
+        clickShowBadReview.innerHTML = `Bad Reviews: ${badReviews.length}`;
+  
+        clickShowBadReview.addEventListener("click", function () {
+          var noReviews = result.filter(function (product) {
+            return product.property_reviews === 0;
+          });
+          renderProductCards(noReviews);
+        });
+
+
+
+
+    function renderProductCards(products) {
+      productContainer.innerHTML = "";
+      products.forEach(function (product) {
+        // Date day month year
+        var createdAt = new Date(product.createdAt);
+        var date = createdAt.toLocaleString("default", { day: 'numeric', month: 'long', year: 'numeric' });
+
+        // Star Solid Regular
+        var card = document.createElement("div");
+        var ratingStars = "";
+        var filledStars = product.property_reviews;
+        var emptyStars = 5 - filledStars;
+
+        for (var i = 0; i < filledStars; i++) {
+          ratingStars += '<i class="icon fa-solid fa-star"></i>';
+        }
+        for (var i = 0; i < emptyStars; i++) {
+          ratingStars += '<i class="icon fa-regular fa-star"></i>';
+        }
+
+        // innerHTML 
+        var card = document.createElement("div");
+        card.innerHTML = `<div class="product-card">
+        <img class="img" src="${product.image[product.image.length - 1]}" />
+        <div class="container">
+            <div class="product-title">${product.title.slice(0, 15)}</div>
+            <div class="product-price">${product.price[0].number + " " + product.price[0].currency.slice(0, 1)}</div>
+          </div>
+            <div class="product-location">${product.location}</div>
+        <div class="price">
+        </div>
+        <hr>
+        <div class="dateRating">
+            <div class="product-currDate">${date}</div>
+            <div class="product-rating">${ratingStars}</div>
+        </div>
+    </div>`;
+
+        productContainer.appendChild(card);
+
+
+      });
+    }
+    renderProductCards(result);
+  })
+
+  .catch((error) => console.log(error));
 
